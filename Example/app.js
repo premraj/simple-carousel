b7b1765efc9e044ca6c102acff1da4216eae6cd5
Prem Raj <premraj0687@gmail.com>
2022-08-16 04:08:07 +0800

    Add files via upload

---
+++ Example/app.js
@@ -0,0 +1,16 @@
+// Here's a usage example to see the carousel in action
+
+PRAJ87Carousel({
+    imageArr: [
+        'https://images.pexels.com/photos/5634773/pexels-photo-5634773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/5827880/pexels-photo-5827880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    ],
+    carouselWidth: 800,
+    transitionSpeed: 400,
+    // isLooped: false,
+    // isAutoPlay: true,
+    // autoPlayDuration: 1500,
+    // nextButtonText: '>',
+    // previousButtonText: '<',
+});

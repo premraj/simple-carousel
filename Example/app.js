934058034e0a8119466cd6253ce9fff4d5b3c313
Prem Raj <premraj0687@gmail.com>
2022-08-16 11:26:39 +0800

    Add files via upload

--- Example/app.js
+++ Example/app.js
@@ -1,16 +1,33 @@
 // Here's a usage example to see the carousel in action
 
 PRAJ87Carousel({
+    carouselDivId: 'my-carousel-looped',
     imageArr: [
         'https://images.pexels.com/photos/5634773/pexels-photo-5634773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         'https://images.pexels.com/photos/5827880/pexels-photo-5827880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     ],
-    carouselWidth: 800,
-    transitionSpeed: 400,
-    // isLooped: false,
-    // isAutoPlay: true,
-    // autoPlayDuration: 1500,
-    // nextButtonText: '>',
-    // previousButtonText: '<',
+});
+
+PRAJ87Carousel({
+    carouselDivId: 'my-carousel-unlooped',
+    imageArr: [
+        'https://images.pexels.com/photos/5634773/pexels-photo-5634773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/5827880/pexels-photo-5827880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    ],
+    isLooped: false,
+    nextButtonText: '>',
+    previousButtonText: '<',
+});
+
+PRAJ87Carousel({
+    carouselDivId: 'my-carousel-autoplay',
+    imageArr: [
+        'https://images.pexels.com/photos/5634773/pexels-photo-5634773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/5827880/pexels-photo-5827880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    ],
+    isAutoPlay: true,
+    autoPlayDuration: 1500,
 });

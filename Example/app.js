e080513f50fd2844c118404184f5ff6c756d2d4b
Prem Raj <premraj0687@gmail.com>
2022-08-19 17:08:13 +0800

    Add files via upload

--- Example/app.js
+++ Example/app.js
@@ -5,7 +5,7 @@ PRAJ87Carousel({
     imageArr: [
         'https://images.pexels.com/photos/5634773/pexels-photo-5634773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         'https://images.pexels.com/photos/5827880/pexels-photo-5827880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-        'https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        'https://images.pexels.com/photos/5834074/pexels-photo-5834074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     ],
 });
 
